6c322dfec630e5ff4fef7d7662f55343fa8d08d4
hyKim <rlagjsdud3@hansung.ac.kr>
2026-02-09 21:35:40 +0900

    fix: Update currentSWVersion to 2.5.14 in index.html

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"db373eb85a06eb3d706af362a45d42d0972f4b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2986061390"
+    serviceWorkerVersion: "2186793002"
   }
 });
